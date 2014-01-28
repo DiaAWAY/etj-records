5a59b1cf39e06f7c2e92e2a8975407b8a311beaf
DiaAWAY <sindrerh@hotmail.com>
2014-01-28 04:09:51 +0100

    ideas and testing

---
+++ public/javascript/album.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by DiaAWAY on 28/01/14.
+ */
+
+class Album {
+
+    AlbumTest(text:string) {
+        return "test: " + text;
+    }
+
+}
+
+function test() {
+    return "albumtest";
+}
+
+var user = test();
